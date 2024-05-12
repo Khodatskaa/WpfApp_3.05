680e7acc3f0a52b3baaaa65f72ca0e563b91b8d9
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-13 00:26:01 +0300

    HW was done

--- Data/DBManager.cs
+++ Data/DBManager.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data
 {
-    internal class DBManager
+    public class DBManager
     {
+        private readonly string connectionString;
+
+        public DBManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                DataTable dataTable = new DataTable();
+                SqlDataReader reader = command.ExecuteReader();
+                dataTable.Load(reader);
+                return dataTable;
+            }
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
